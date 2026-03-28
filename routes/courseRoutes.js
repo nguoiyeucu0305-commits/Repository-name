6d2ae2fdc16d1a942d668ea4b0a395a77dd771ae
Quang Minh <nguoiyeucu0305@gmail.com>
2026-03-29 00:59:15 +0700

    update code

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -2,6 +2,20 @@ const express = require("express");
 const router = express.Router();
 const courseController = require("../controllers/courseController");
 
+// Danh sách khóa học
 router.get("/courses", courseController.getCourses);
-module.exports = router;
+
+// Trang chi tiết khóa học (id động)
 router.get("/course/:id", courseController.getCourseDetail);
+
+// Trang học
+router.get("/learn/:id", (req, res) => {
+    const id = req.params.id;
+    // Nếu bạn có dữ liệu từ DB thì thay bằng Course.findById(id)
+    const course = courses.find(c => c.id == id);
+
+    res.render("learn", { course });
+});
+
+// Xuất router sau khi định nghĩa xong
+module.exports = router;
--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-
+const mongoose = require("mongoose");
 // template engine
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../views"));
 
 // static files
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../public")));
 
 // routes
 const courseRoutes = require("../routes/courseRoutes");
@@ -17,8 +17,22 @@ app.use("/", courseRoutes);
 app.get("/", (req, res) => {
     res.render("home");
 });
+app.get("/login",(req,res)=>{res.render("login");});
+app.post("/login", (req, res) => {
+    const { username, password } = req.body;
 
+    if (username === "admin" && password === "123") {
+        res.send("Đăng nhập thành công");
+    } else {
+        res.send("Sai tài khoản");
+    }
+});
 // server
 app.listen(3000, () => {
     console.log("Server running at http://localhost:3000");
 });
+
+// Kết nối MongoDB
+mongoose.connect("mongodb://127.0.0.1:27017/Course")
+  .then(() => console.log("Kết nối MongoDB thành công!"))
+  .catch(err => console.log("Lỗi kết nối:", err));
